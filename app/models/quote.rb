d498fbf60424658619bc4c2ad4955e1d8c4b66c6
Roland Martinet <r.martinet@nerya.fr>
2024-05-28 22:23:26 +0200

    empty_states_with_hotwire

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,7 +7,5 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(created_at: :desc) }
 
-  after_create_commit -> { broadcast_prepend_later_to { [quote.company, 'quotes'] } }
-  after_update_commit -> { broadcast_replace_later_to { [quote.company, 'quotes'] } }
-  after_destroy_commit -> { broadcast_remove_to { [quote.company, 'quotes'] } }
+  broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
 end
